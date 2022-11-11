b0376270eef77ad706d4857ef880e93f56d360ed
B Shiva Kumar <92181068+B-Shiva-Kumar@users.noreply.github.com>
2022-11-11 21:35:45 +0530

    Add files via upload

---
+++ miscellaneous/pointers.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "bill"
+
+	namePointer := &name
+
+	fmt.Println(&namePointer)
+	printPointer(namePointer)
+}
+
+func printPointer(namePointer *string) {
+	fmt.Println(&namePointer)
+}
+
+// The log statements will print different addresses because "everything" in Go is pass by value.
